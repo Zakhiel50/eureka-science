d93a00d56e74f0f03977ede78740cd41d1c73cb2
Luc Rigaud <lucrigaud50@gmail.com>
2026-06-15 13:57:24 +0200

    feat: add sun-course

--- app/manifest.ts
+++ app/manifest.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "EUREKA : L'Odyssée des Sciences",
-    short_name: 'EUREKA',
+    short_name: 'EUREKA-1.0.0',
     description: "Apprendre les sciences en s'amusant",
     start_url: '/',
     display: 'standalone',
